f517b25de77307342df3e43da37e43797e95359e
Henrymoky <jhonmoki17@gmail.com>
2024-10-11 20:02:58 +0800

    Add files via upload

--- latihan catatan/Pertemuan 5/list and tuple.py
+++ latihan catatan/Pertemuan 5/list and tuple.py
@@ -1,6 +1,73 @@
-#list barang
-barang = [ [“Sapatu”, “Kaus Kakhi”, “Sarung”], [“Pulpen”, “Pensil”, “Laptop”] ]
-#memanggil list secara keseluruhan untuk melihat bagaimana outputnya print(barang, “\n”)
-#memanggil indeks pertama print(barang[0], “\n”)
-#kali ini kita ingin menampilkan elemen “Pulpen”, kita perlu mengakses indeks dua kali, yang pertama untuk memilih list dengan elemen yang kita inginkan dan yang kedua indeks elemen itu sendiri
-print(barang[1][0])
+# list1 = ["a",
+#          2, 
+#          True, 
+#          [1,2,3]]
+
+# print(type(list1[-1])[2])
+
+tas = ["buku", 32, True, 3.14, ["IF", 24]]
+# for i in tas:
+#     print(i)
+# print(len(tas))
+
+# for i in range(len(tas)):
+#     print(tas[i])
+    
+matkul = ["Kalkulus", "Fisdas", "Pengantar Teknologi Informasi"]
+# print(matkul)
+
+# matkul.append("Logika Informatika")
+# print(matkul)
+
+# matkul.insert(1, "sastra mesin")
+# print(matkul)
+
+# matkul[1] = "fisika quantum"
+# print(matkul)
+
+# del matkul[1]
+# print(matkul)
+
+# matkul.pop(1)
+# print(matkul)
+
+# buang = matkul.pop(1)
+# print(f"variabel buang = {buang}")
+
+# prodi = ["Informatika", "Sistem Informasi", "Teknik Arsitektur", "Teknik Lingkungan", "Teknik Pertambangan", "Teknik Elektro", "Teknik Industri", "Teknik SIpil", 
+#          "Teknik Geologi", "Teknik Kimia"]
+# print(prodi[::2], "\n", prodi)
+
+# bensin = ["avanza", "honda", "yamaha"]
+# listrik = ["tesla", "SAIC"]
+# semua = bensin+listrik
+# print(semua)
+# print(bensin*3)
+
+# barang = [["sepatu", 'tas', 'baju'], 
+#           ['pulpen', 'pensil', 'laptop']]
+# # print(barang[1][1])
+# print(barang)
+# for baris in barang:
+#     for kolom in baris:
+#         print(kolom)
+
+# nama = ("aldi", 'daffa', "arisya")
+# print(nama)
+
+# barang1 = ["sepatu", 'tas', 'baju'] 
+# barang2 = ['pulpen', 'pensil', 'laptop']
+# barangtotal = [barang1, barang2]
+# print(barangtotal)
+
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+# #lalu kita unpacking
+# absen, prodi, nim, nama = mahasiswa
+# #maka ketiga variabel tersebut akan berisi data dari tuple mahasiswa
+# #menampilkan variabel
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+
+#mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
