9ee6300b4eb26f437971085079dbb01b6f30f78d
Henrymoky <jhonmoki17@gmail.com>
2024-10-18 15:19:18 +0800

    Add files via upload

---
+++ latihan catatan/pertemuan 7/fungsi dan prosedur.py
@@ -0,0 +1,67 @@
+# def nama_fungsi():
+# print ("hello world")
+
+# def tambah (a,b):
+#     hasil = a + b
+
+#     print(hasil)
+
+# tambah(10)
+
+# def tambah(a,b):
+#     hasil= a+b
+#     return hasil
+
+# tambahan = tambah(2,4)+tambah(4,6)
+
+# print(tambahan)
+
+# def pilihan(opsi):
+#     match opsi :
+#         case 1 :
+#             print(1)
+#             return
+#         case 2 :
+#             print(2)
+#             return
+#     print(3)
+
+# pilihan(2)   
+
+# 
+# def cetak(nama,nim,matkul):
+#     print(nama,nim,matkul)
+
+# cetak("henry",41,matematika=100)
+
+# var=2,4,5
+
+# var1,var2,var3 = var
+
+# print
+
+
+# import os
+# os.system("cls")
+
+
+# nama= "informatika"
+# mata_kuliah+ "algoritma dan pemrograman dasar"
+# kelas = "IT asik"
+
+# def info():
+#     nama= "Teknik Elektro"
+#     mata_kuliah= "pengantar teknik elektro"
+#     var= 1000
+#     print("prodi:",nama)
+#     print("mata kuliah:", mata_kuliah)
+
+    #dalamdef(10)
+
+# def cetak(nama,nim, umur=18):
+#     print(nama,nim,umur)
+
+# if __name__ == "__main__":
+#     def cetak(nama,nim, umur=18):
+#     cetak(nama,nim,umur)
+
